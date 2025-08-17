f67a17f78b3b018e0b9a5ac9ef07798ad58369ca
arash039 <shojaa.arash@gmail.com>
2025-08-17 16:32:36 +0200

    feat(database.py): add event sponsorship
    - Implemented many-to-many relationship
    
    - Improved model structure

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Float, ForeignKey, Table
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Column, Float, ForeignKey, Table, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
 	pass
@@ -11,3 +11,38 @@ class Ticket(Base):
 	price: Mapped[float] = mapped_column(nullable=True)
 	show: Mapped[str | None]
 	user: Mapped[str | None]
+	sold: Mapped[bool] = mapped_column(default=False)
+	details: Mapped["TicketDetails"] = relationship(back_populates="ticket")
+	event_id: Mapped[int | None] = mapped_column(ForeignKey("events.id"))
+	event: Mapped["Event | None"] = relationship(back_populates="tickets")
+
+class TicketDetails(Base):
+	__tablename__ = "ticket_details"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	ticket_id: Mapped[int] = mapped_column(ForeignKey("tickets.id"))
+	ticket: Mapped["Ticket"] = relationship(back_populates="details")
+	seat: Mapped[str | None]
+	ticket_type: Mapped[str | None]
+
+class Event(Base):
+	__tablename__ = "events"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	name: Mapped[str]
+	tickets: Mapped[list["Ticket"]] = relationship(back_populates="event")
+	sponsors: Mapped[list["Sponsor"]] = relationship(secondary="sponsorships", back_populates="events")
+
+class Sponsor(Base):
+	__tablename__ = "sponsors"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	name: Mapped[str] = mapped_column(unique=True)
+	events: Mapped[list["Event"]] = relationship(secondary="sponsorships", back_populates="sponsors")
+
+class Sponsorship(Base):
+	__tablename__ = "sponsorships"
+
+	event_id: Mapped[int] = mapped_column(ForeignKey("events.id"), primary_key=True)
+	sponsor_id: Mapped[int] = mapped_column(ForeignKey("sponsors.id"), primary_key=True)
+	amount: Mapped[float] = mapped_column(nullable=False, default=10)
